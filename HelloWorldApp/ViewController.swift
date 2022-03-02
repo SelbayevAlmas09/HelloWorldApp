684073de00e8a545588d5b0aab71a997e389b03f
Селбаев Алмас <almasselbayev@gmail.com>
2022-03-02 22:46:16 +0600

    Work with logic

--- HelloWorldApp/ViewController.swift
+++ HelloWorldApp/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var greetingLabel: UILabel!
+    @IBOutlet var greetingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        greetingLabel.isHidden = true
+        greetingButton.configuration = setupButton(with: "Show Greeting")
+        //greetingButton.layer.cornerRadius = 10
     }
 
-
+    /*
+    @IBAction func greetingButtonPressed() {
+        greetingLabel.isHidden.toggle()
+        greetingButton.setTitle(
+            greetingLabel.isHidden ? "Show Greeting" : "Hide Greeting",
+            for: .normal
+        )
+    }
+    */
+    @IBAction func greetingButtonPressed() {
+        greetingLabel.isHidden.toggle()
+        greetingButton.configuration = setupButton(
+            with: greetingLabel.isHidden ? "Show Greeting" : "Hide Greeting")
+    }
+    
+    private func setupButton(with title: String) -> UIButton.Configuration {
+        var buttonConfiguration = UIButton.Configuration.filled()
+        buttonConfiguration.baseBackgroundColor = #colorLiteral(red: 1, green: 0.02739950456, blue: 0.3375478089, alpha: 1)
+        buttonConfiguration.title =  title
+        buttonConfiguration.buttonSize = .large
+        buttonConfiguration.attributedTitle?.font = UIFont.systemFont(ofSize: 24)
+        buttonConfiguration.cornerStyle = .large
+        
+        return buttonConfiguration
+    }
 }
 
